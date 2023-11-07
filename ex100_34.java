821d1b157f80be6bc86b6d45e89ebc236f786c8c
JC-soft <74031502+JC-soft@users.noreply.github.com>
2023-11-07 14:31:28 +0900

    Create ex100_34.java

---
+++ ex100_34.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int a;
+        do{
+            a = scan.nextInt();
+            System.out.println(a);
+        }while(a!=0)
+        
+    }
+}
